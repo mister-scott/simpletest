7c8bcfa5c17b46961c0054794d22871a167ebc33
Scott McGimpsey <scott.mcgimpsey@aei.com>
2024-08-02 08:10:52 -0600

    Add example for selecting files or folders

---
+++ tests/test_directory_file_select.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def maintest(settings, test_series, *args, **kwargs):
+    root = tk.Tk()
+    root.withdraw()
+    
+    file_path = filedialog.askopenfilename()
+
+    if file_path:
+        print(f"Directory passed:{file_path}!")
+        return "pass"
+    else:
+        dir_path = filedialog.askdirectory()
+    
+    
+    if dir_path:
+        print(f"Directory passed:{dir_path}!")
+        return "pass"
+    else:
+        print("No name entered.")
+        return "fail"
